4942f4ae433459fbfe1d2cabafc81f1648fb27e7
MainDab <50540449+MainDabRblx@users.noreply.github.com>
2020-09-21 15:28:35 +0800

    v

---
+++ Main's Experimental Chat System/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Main_s_Experimental_Chat_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Carlito", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(5, 33);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(350, 127);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "-- This is the start of the chat --";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(5, 165);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(349, 13);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Type your message in here..";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.e);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(367, 24);
+            this.panel1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(327, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "-";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(348, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(23, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MainDab Chat System";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(21, 21);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this.panel1;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.label4.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(95, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Send Message!";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.label5.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(200, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Settings";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(5, 214);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(253, 13);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Type your username here";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.label6.Font = new System.Drawing.Font("Corbel", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(277, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 14);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Set name";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.label7.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(5, 231);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 18);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Clear text";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.ClientSize = new System.Drawing.Size(365, 247);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "MainDab Chatbox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ Main's Experimental Chat System/Form1.cs
@@ -0,0 +1,238 @@
+﻿using Discord;
+using Discord.Gateway;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Main_EX#3898 @ discord.io/maindab or yohatipynoo@gmail.com
+namespace Main_s_Experimental_Chat_System
+{
+        // PLEASE MAKE SURE TO USE A USER TOKEN. THIS DOES NOT SUPPORT BOTS.
+    public partial class Form1 : Form
+    {
+        int isopen = 0; // Varible for the settings tab below
+        string yesnt = ""; // This variable is for saving the previous chat
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new Size(365, 207); // Puts it back to original size
+            try // If thing.txt is found. Thing.txt is used to store messages that are recieved.
+            {
+                if (File.Exists("thing.txt"))
+                {
+                    File.Delete("thing.txt"); // Noone wants to see the old text
+                }
+            }
+            catch // Just so the user won't see any errors.
+            {
+
+            }
+            if (!File.Exists("thing.txt")) // if thing.txt dosen't exist
+            {
+
+                File.Create("thing.txt"); // create it
+                
+            }
+            if (!File.Exists("username.txt")) // if username.txt dosen't exist. username.txt is for storing the username of the user
+            {
+
+                File.Create("username.txt");
+                File.WriteAllText("username.txt", "User"); // write to file
+
+            }
+            yesnt = richTextBox1.Text; // saving previous data
+            string sdafnasjkdfasdf = File.ReadAllText("username.txt"); // creates a varible for annoncing someone has joined
+            DiscordClient client = new DiscordClient("NzU2NTA2NjAzODA0Njg4NTQ1.X2S1rw.yxhRepyxTR7wDos-au9kvqvrR9w"); // paste ur token here. must be a user token.
+            DiscordMessage message = client.SendMessage(757426913643069490, sdafnasjkdfasdf + " is now on!", false); // tells eveyrone someone is on
+            // change 757426913643069490 with ur channel id
+            try
+            {
+                this.backgroundWorker1.RunWorkerAsync(); // runs some code
+       
+                timer2.Enabled = true; // starts another thing
+            }
+            catch
+            {
+            }
+
+        }
+        string rich = "";
+
+        private static void client_OnMessageReceived(DiscordSocketClient client, MessageEventArgs args) // When the appplications revieces a message. suggested that the user you are logged into can only see one channel.
+        {
+            string someText = (args.Message.Content); // text recieved
+            string oldtext = ""; // old text
+            System.Threading.Thread.Sleep(100);
+            if (oldtext == someText) // here i attempted to make it check if the sane nessage has been sent
+            {
+                DiscordMessage message = client.SendMessage(757426913643069490, "Hello, World", false); // ignore this code.
+                message.Delete();
+                message.Delete();
+            }
+            else
+            {
+                File.WriteAllText("thing.txt", someText); // else write to the text
+                oldtext = someText;
+            }
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e) // auto scoll so you don't have to always scroll
+        {
+            richTextBox1.SelectionStart = richTextBox1.Text.Length; 
+            // scroll it automatically
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e) // loop code
+
+        {
+            try // just so user dosent see errors
+            {
+                yesnt = richTextBox1.Text;// saves message logs 
+                string readText = File.ReadAllText("thing.txt"); // thing.txt contains the most recent message
+                if (rich == readText)
+                {
+                    // suggested you don't do anything here, this just checks if a user sent the same thing again. do not remove this part!
+                }
+                else
+                {
+                    richTextBox1.Text = ""; // clear textbox
+                    richTextBox1.Text = (yesnt + "\n" + readText); // previous message logs + new line + new text
+                    rich = readText; // saves the current recieved message as a string
+                }
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) // just so the client dosen't lag
+        {
+            DiscordSocketClient client = new DiscordSocketClient();
+            client.OnMessageReceived += client_OnMessageReceived; // check for messages
+            client.Login("NzU2NTA2NjAzODA0Njg4NTQ1.X2S1rw.yxhRepyxTR7wDos-au9kvqvrR9w"); // put ur user token here
+        }
+
+        private void label2_Click(object sender, EventArgs e) // when x pressed
+        {
+            string sdafnasjkdfasdf = File.ReadAllText("username.txt"); // reads for username
+            DiscordClient client = new DiscordClient("NzU2NTA2NjAzODA0Njg4NTQ1.X2S1rw.yxhRepyxTR7wDos-au9kvqvrR9w"); // put ur user token here
+            DiscordMessage message = client.SendMessage(757426913643069490, sdafnasjkdfasdf + " has left.", false); // tellls people someone left
+            // change 757426913643069490 with ur channel id
+            Environment.Exit(0); // obvious
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            base.WindowState = FormWindowState.Minimized; // obvious 
+        }
+
+        private void label4_Click(object sender, EventArgs e) // send button, alternatively the user can press enter to send
+        {
+            try // just so user dosen't see error
+            {
+                this.backgroundWorker2.RunWorkerAsync(); // run code
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // nothing here
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e) // changing username button
+        {
+            if (textBox2.Text == "Type your username here") // just so it isn't blank
+            {
+                MessageBox.Show("Please enter a vaild username!", "MainDab Chat System BETA");
+            }
+            else if (textBox2.Text == "") // just so it isn't blank
+            {
+                MessageBox.Show("Please enter a vaild username!", "MainDab Chat System BETA");
+            }
+            else // success
+            {
+                File.WriteAllText("username.txt", textBox2.Text);
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e) // settings bar
+        {
+            if (isopen == 0) // if the setings is closed
+            {
+                this.Size = new Size(365, 247);
+                
+                isopen = 1; // sets the settings to open
+            }
+            else // if isopen!= 0 then (if already open)
+            {
+                this.Size = new Size(365, 207); // then close
+                isopen = 0; // sets varible to show its closed
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = ""; // clear chat, idk why you would though.
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string no = File.ReadAllText("username.txt"); // read username
+            DiscordClient client = new DiscordClient("NzU2NTA2NjAzODA0Njg4NTQ1.X2S1rw.yxhRepyxTR7wDos-au9kvqvrR9w"); // paste ur user token here
+            if (textBox1.Text == "") // just so user dosen't send blank...
+            {
+                MessageBox.Show("Please enter a message!", "MainDab Chat System BETA"); // tells them
+            }
+            else if (textBox1.Text == "Type your message in here..") // also just to make sure they don't send the example message
+            {
+                MessageBox.Show("Please enter a message!", "MainDab Chat System BETA"); // tells them, be user friendly!
+            }
+            else
+            {
+                DiscordMessage message = client.SendMessage(757426913643069490, ("[" + no + "] " + textBox1.Text + "\n"), false); // sends message to discord
+                // change 757426913643069490 to ur channel id
+
+                textBox1.Text = ""; // clears textbox
+            }
+        }
+
+        private void e(object sender, KeyEventArgs e) // when the enter key is pressed, so the send message dosen't have to be clicked all the time
+        {
+            if (e.KeyCode == Keys.Return) // if enter key pressed
+            {
+                
+                try
+                {
+                    this.backgroundWorker2.RunWorkerAsync(); // run code
+                    
+                }
+                catch
+                {
+
+                }
+                e.Handled = true; // so no error sounds play while clicked.
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+// that is pretty much it, i don't mind if you skid it but just make sure to give credits.
